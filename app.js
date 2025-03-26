e2b8131185e3438891c3615ad6df0c9652b318d7
iahmadhabibx <iahmadhabibx@gmail.com>
2025-03-26 21:21:49 +0500

    feat: complete working of dragging and sorting

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
 
+const getDragAfterElement = (container, y) => {
+    const draggableElements = [
+      ...container.querySelectorAll('.draggable:not(.dragging)'),
+    ];
+  
+    return draggableElements.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+          return { offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      {
+        offset: Number.NEGATIVE_INFINITY,
+      }
+    );
+  };
+
 draggables.forEach((draggable) => {
   draggable.addEventListener('dragstart', () => {
     draggable.classList.add('dragging');
@@ -16,31 +37,10 @@ containers.forEach((container) => {
     const afterElement = getDragAfterElement(container, dragoverEvent.clientY);
     const draggable = document.querySelector('.dragging');
 
-    if (!afterElement) {
+    if (afterElement == null) {
       container.appendChild(draggable);
     } else {
-      container.insertBefore(draggable, afterElement);
+      container.insertBefore(draggable, afterElement.element);
     }
   });
 });
-
-const getDragAfterElement = (container, y) => {
-  const draggableElements = [
-    ...container.querySelectorAll('.draggable:not(.dragging)'),
-  ];
-
-  return draggableElements.reduce(
-    (closest, child) => {
-      const box = child.getBoundingClientRect();
-      const offset = y - box.top - box.height / 2;
-      if (offset < 0 && offset > closest.offset) {
-        return { offset, element: child };
-      } else {
-        return closest;
-      }
-    },
-    {
-      offset: Number.NEGATIVE_INFINITY,
-    }
-  );
-};
